2e44d20c3efb2e6ac4597cb822a44c28626b49f5
evgen <evgenijvy@gmail.com>
2022-12-13 21:37:29 +0200

    task3-3_v1_math_to_js

---
+++ task3-3_v1_math_to_js/task3-3_v1_math_to_js.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let angle;
+do {
+    let inputValue = prompt('Введите угол в радианах', '0');
+    angle = calculateAngle(inputValue);
+} while (angle === undefined)
+if (angle !== null) {
+    alert(angle);
+}
+
+function calculateAngle(countRadian) {
+    if (countRadian === null) {
+        return null;
+    }
+
+    if (Number(countRadian) != countRadian) {
+        alert('Вводить нужно число!');
+        return undefined;
+    }
+
+    let isNegative = countRadian < 0;
+    countRadian = Math.abs(countRadian);
+
+    let degrees = Math.trunc(countRadian * 180 / Math.PI);
+    let remainder = countRadian * 180 / Math.PI - degrees;
+    let minutes = Math.trunc(remainder * 60);
+    let seconds = Math.round(remainder * 3600 - minutes * 60);
+    return ((isNegative) ? '-' : '') + degrees + '°' + minutes + "'" + seconds + '"';
+}
