50ebf89a2f8312bba9d8182c544ae7449b08df75
evgen <evgenijvy@gmail.com>
2022-12-14 15:53:35 +0200

    add validation

--- task3-1_v1_math_to_js/task3-1_v1_math_to_js.js
+++ task3-1_v1_math_to_js/task3-1_v1_math_to_js.js
@@ -3,13 +3,17 @@
 let m = prompt('Input M');
 let confirmed = '';
 
-for (let x = 0; x <= m ** (1 / 3); x++) {
-    for (let y = 0; y <= (m - x ** 3) ** (1 / 3); y++) {
-        for (let z = 0; z <= (m - x ** 3 - y ** 3) ** (1 / 3); z++) {
-            if (x ** 3 + y ** 3 + z ** 3 == m) {
-                confirmed += '(' + x + ', ' + y + ', ' + z + ')' + '\n';
+if (isNaN(Number(m)) || m % 1 != 0) {
+    alert('You can input natural number only!');
+} else {
+    for (let x = 0; x <= m ** (1 / 3); x++) {
+        for (let y = 0; y <= (m - x ** 3) ** (1 / 3); y++) {
+            for (let z = 0; z <= (m - x ** 3 - y ** 3) ** (1 / 3); z++) {
+                if (x ** 3 + y ** 3 + z ** 3 == m) {
+                    confirmed += '(' + x + ', ' + y + ', ' + z + ')' + '\n';
+                }
             }
         }
     }
+    alert(confirmed);
 }
-alert(confirmed);
